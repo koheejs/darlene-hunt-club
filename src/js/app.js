ed8402a1975631c789956698c87f4358e931e100
koheejs <dangkhoibr1992@gmail.com>
2024-03-30 23:34:55 +0700

    feat: implement menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,23 @@
 (function () {
+  const menuTrigger = document.getElementById('trigger-menu');
+  const navigation = document.getElementById('navigation');
+
+  const Classes = {
+    menuExpanded: 'expanded',
+  };
+
+  menuTrigger.addEventListener('click', () => {
+    navigation.classList.toggle(Classes.menuExpanded);
+  });
+
+  document.body.addEventListener('click', (e) => {
+    if (e.target !== menuTrigger && !menuTrigger.contains(e.target)) {
+      navigation.classList.remove(Classes.menuExpanded);
+    }
+  });
+})();
+
+(function () {
   const accordions = document.querySelectorAll('[data-accordion]');
   accordions.forEach(function (accordion) {
     const title = accordion.querySelector('.accordion-title');
