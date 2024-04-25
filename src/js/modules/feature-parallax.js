2c4c2def6f4f2bc37b67959fed3ef01aff946578
koheejs <dangkhoibr1992@gmail.com>
2024-04-26 00:33:30 +0700

    feat: update section parallax effect

--- src/js/modules/feature-parallax.js
+++ src/js/modules/feature-parallax.js
@@ -1,16 +1,71 @@
+const DELTA_1 = 400;
+const KEEP_DELTA = 1000;
+
 function sectionParallax() {
-  const $sections = document.querySelectorAll('[data-scroll-speed]');
+  const $section = document.getElementById('descriptions');
+  const row1 = $section.querySelector('.row:nth-child(1)');
+  const row2 = $section.querySelector('.row:nth-child(2)');
 
-  $sections.forEach(function ($section) {
-    const speed = $section.getAttribute('data-scroll-speed');
-    const originMarginTop = parseInt($section.style.marginTop, 10) || 0;
+  const upperLimit1 = window.innerHeight * 0.8;
+  const upperLimit2 = upperLimit1 + 2 * DELTA_1 + 2 * KEEP_DELTA;
 
-    window.addEventListener('scroll', function () {
-      const scrollY = window.scrollY;
-      const updatedMarginTop = originMarginTop + scrollY * speed * -1;
-      $section.style.marginTop = `${updatedMarginTop}px`;
-      AOS.refresh();
-    });
+  $section.style.height = `${
+    window.innerHeight + upperLimit2 + 2 * DELTA_1 + KEEP_DELTA
+  }px`;
+
+  const sectionTop = $section.offsetTop;
+  const sectionHeight = $section.offsetHeight;
+  const sectionBottom = sectionTop + sectionHeight;
+
+  // Parallax effect for the background image
+  function parallaxBackground() {
+    if (scrollY > sectionTop) {
+      $section.classList.add('bg-bottom');
+    } else {
+      $section.classList.remove('bg-bottom');
+    }
+
+    if (
+      scrollY >= sectionTop &&
+      scrollY + window.innerHeight <= sectionBottom
+    ) {
+      $section.classList.add('parallax');
+    } else {
+      $section.classList.remove('parallax');
+    }
+  }
+
+  // Parallax effect for the rows
+  function parallaxRow($el, upperLimit, delta) {
+    if ($el) {
+      if (
+        scrollY >= upperLimit &&
+        scrollY <= upperLimit + 2 * delta + KEEP_DELTA
+      ) {
+        let opacity = 0;
+        if (scrollY <= upperLimit + delta) {
+          opacity = Math.abs(upperLimit - scrollY) / delta;
+        } else if (scrollY >= upperLimit + delta + KEEP_DELTA) {
+          opacity =
+            Math.abs(upperLimit + 2 * delta + KEEP_DELTA - scrollY) / delta;
+        } else {
+          opacity = 1;
+        }
+        $el.style.opacity = opacity;
+      } else {
+        $el.style.opacity = 0;
+      }
+    }
+  }
+
+  window.addEventListener('scroll', parallaxBackground);
+
+  window.addEventListener('scroll', () => {
+    parallaxRow(row1, upperLimit1, DELTA_1);
+  });
+
+  window.addEventListener('scroll', () => {
+    parallaxRow(row2, upperLimit2, DELTA_1);
   });
 }
 
